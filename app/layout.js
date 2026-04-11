ed2f9d3aef24ff7521160bd758cc51d9e3dacd71
Bisan <your@email.com>
2026-04-11 18:41:29 +0200

    fix viewport

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,13 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Lager Pro",
+  description: "Lager Pro Web",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="ar">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </head>
       <body>{children}</body>
     </html>
   );
