219ebd07347cc92e55ec2bb245a116a8533707b1
Bisan <your@email.com>
2026-05-09 21:39:16 +0200

    rename app to Warehouse

--- app/layout.js
+++ app/layout.js
@@ -9,8 +9,7 @@ export default function RootLayout({ children }) {
         <meta name="theme-color" content="#2D3E50" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="Lager Pro" />
-        <link rel="apple-touch-icon" href="/icon-192.png" />
+<meta name="apple-mobile-web-app-title" content="Warehouse" />        <link rel="apple-touch-icon" href="/icon-192.png" />
       </head>
       <body>{children}</body>
     </html>
